d1a447ef14f646ad0bcd49f882910a604901b6b5
Zawad Dewan <zawaddewan@gmail.com>
2022-03-20 16:18:33 -0400

    added a function to grab the hourly demand data from eia and then only take a year's worth to put into a dictionary, will generalize for other states later

---
+++ parsers/HourlyDemandParser.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+url = "http://api.eia.gov/series/?api_key=YOUR_API_KEY_HERE&series_id=EBA.NY-ALL.D.H"
+
+test = requests.request("POST", url)
+
+#Test parser to get all the data for a certain year from new york, will generalize later for all the states
+def getdemandict(response, year):
+    demand = {}
+    data = json.loads(response.text)
+    for thing in data["series"][0]["data"]:
+        if thing[0][:4] == year:
+            demand[thing[0]] = thing[1]
+    return demand
+print(getdemandict(test, "2022"))
