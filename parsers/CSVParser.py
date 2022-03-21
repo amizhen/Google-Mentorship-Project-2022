c3aa642ceadf354de3b1a3b3cf7e55dd40f6604d
Jeffrey Jiang <jjiang30@stuy.edu>
2022-03-21 07:54:52 -0400

    started CSV parser class

--- parsers/CSVParser.py
+++ parsers/CSVParser.py
@@ -1,4 +1,5 @@
 import csv
+from typing import Mapping
 
 """
 Notes:
@@ -12,9 +13,45 @@ CSV FILES THAT I LOVE AND ADMIRE - wave_energy.csv
 CSV FILES THAT WE NEED TO FIX - the rest
 """
 
-filePath = r""
+filePath = open("data\\raw\\csv\\biomass.csv")
 
 file = csv.DictReader(filePath)
 
+class CSVParser:
+
+    def __init__(self, filePath : str):
+        self.filePath = filePath
+        self.csvFile = open(filePath, "r")
+        self.opened = True
+        self.categories = []
+
+    def __call__(self) -> list[Mapping[str, str]]:
+        if self.opened:
+            print(self.filePath)
+            print(self.categories)
+        # reader = csv.DictReader(self.csvFile)
+        # return [{key : row[key] for key in row} for row in reader] # TODO: Fix the dict comprehension
+
+    def addCategory(self, *args : list[str]) -> None:
+        for category in args:
+            if isinstance(category, str) and category not in self.categories:
+                self.categories.append(category)
+
+    def removeCategory(self, *args : list[str]) -> None:
+        for category in args:
+            if isinstance(category, str) and category in self.categories:
+                self.categories.remove(category)
+
+    def clearCategory(self) -> None:
+        self.categories.clear();
+
+    def close(self) -> None :
+        self.csvFile.close()
+        self.opened = False
+
+
 if __name__ == "__main__":
-    print("Hello world")
+    parser = CSVParser("data\\raw\\csv\\biomass.csv")
+    parser.addCategory("Lat")
+
+    parser()
