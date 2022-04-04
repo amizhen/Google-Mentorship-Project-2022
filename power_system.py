f32fb7239e5e5c1e12c0feb56c76ae32c709cc26
Aiden Mizhen <amizhen30@stuy.edu>
2022-04-04 11:37:41 -0400

    Started PowerSystem object, to be used for fitness function as well as potential genetic algorithm

---
+++ power_system.py
@@ -0,0 +1,13 @@
+
+class PowerSys:
+    def __init__(self, amt_solar, amt_wind, amt_storage):
+        self.storage = amt_storage
+        self.wind = amt_wind
+        self.solar = amt_solar
+
+    def __str__(self):
+        return f'({self.solar}, {self.wind}, {self.storage})'
+
+if __name__ == 'main':
+    a = PowerSys(1, 2, 3)
+    print(a)
