0335b0cd2f895f1e5dad4d6e34ac52753a627bd5
Jeffrey Jiang <jjiang30@stuy.edu>
2022-04-21 18:44:51 -0400

    created formula to turn solar data to power

--- API_test.py
+++ API_test.py
@@ -3,8 +3,8 @@ import pprint
 from data.Data import TimeRange, get_electric_demand, get_solar_data, get_wind_data
 
 if __name__ == "__main__":
-    pprint.pprint(get_solar_data(42.355978782625336, -74.9824086324889, date(2020, 1, 1), date(2020, 1, 2)))
-
+    # pprint.pprint(get_solar_data(42.355978782625336, -74.9824086324889, date(2020, 1, 1), date(2020, 1, 2)))
+    pprint.pprint(get_wind_data(42.355978782625336, -74.9824086324889, TimeRange.EVERY_WEEK))
     # pprint.pprint(get_electric_demand("Arizona", 2015))
 
     # t = get_wind_data(42.355978782625336, -74.9824086324889, TimeRange.EVERY_TWO_WEEK)
--- util/Formula.py
+++ util/Formula.py
@@ -20,6 +20,22 @@ def get_wind_power(air_density : float, radius : float, wind_velocity : float, e
 
     return 0.5 * air_density * radius ** 2 * math.pi * wind_velocity ** 3 * efficiency
 
+def get_solar_power(irradiance : float, length : float, width : float, efficiency : float = 0.15) -> float:
+    """
+    Function to calculate the power (joules per second) produced by solar panels
+
+    Args:
+        irradiance : The irradiance (W/m^2)
+        length : length of solar panel
+        width : width of solar panel
+        efficiency : efficiency of solar panel. Range is usually between 15% to 18%
+
+    Returns:
+        The power (J/s) produced by the solar panel
+    """
+
+    return irradiance * length * width * efficiency
 
 if __name__ == "__main__":
-    print(get_wind_power(1.23, 52, 12, 0.4))
+    print(get_solar_power(200, 10, 10))
+    print(get_wind_power(1.23, 52, 13.37, 0.4))
