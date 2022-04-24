6dbef80431b69d35255d4734e83d62cec9a43af8
Aiden Mizhen <amizhen30@stuy.edu>
2022-04-24 14:13:48 -0400

    Started fitness function for powersys

--- power_fitness.py
+++ power_fitness.py
@@ -1,9 +1,11 @@
 from power_system import PowerSys
-from parsers.SolarData import get_solar
-from parsers.HourlyDemandParser import get_demand
 
 
-def test(sys : PowerSys, loc : str) -> int:
-    # Need dict for converting location to state or electrical grid region
-    # solar_gen_data = get_solar(0, 0)[avg_whatever][monthly] # Need to decide between dni and ghi
-    # usage_data = get_demand(loc, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)
+def test(sys : PowerSys, loc : str) -> None:
+    """
+    :param sys: Takes a power setup
+    :param loc: Takes a location
+    :return: modifies the fitness parameter of the input power system (maybe returns other useful info about run)
+    """
+    return
+
