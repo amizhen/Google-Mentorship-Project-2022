16040739c2ac1bc68ac099d3bbc93011bae09944
Aiden Mizhen <amizhen30@stuy.edu>
2022-04-26 22:57:59 -0400

    Tweaked instance vars

--- power_system.py
+++ power_system.py
@@ -1,12 +1,19 @@
 class PowerSys:
     def __init__(self, amt_solar, amt_wind, amt_storage):
-        self.storage = amt_storage
+        self.storage_cap = amt_storage
+        self.stored = amt_storage
         self.wind = amt_wind
         self.solar = amt_solar
         self.fitness = 0
+        self.gen_history = {}  # Chart of time vs power generated at that time
+        # format = time:(wind, solar)
+        self.storage_history = {}  # Chart of time vs amt of power in storage, shows deficit for the hour if negative
+        # format = time:net
 
     def __str__(self):
-        return f'({self.solar}, {self.wind}, {self.storage}, {self.fitness})'
+        return f'({self.solar}, {self.wind}, {self.storage_cap}'
+
+    # Makes PowerSys sortable by the fitness score
 
     def __lt__(self, other):
         return self.fitness < other.fitness
@@ -30,4 +37,4 @@ class PowerSys:
 if __name__ == '__main__':
     a = PowerSys(1, 2, 3)
     print(a)
-    print(a.storage)
+    print(a.stored)
