202b1133ecec086e6b21e4af990ebc968bd9d1b3
Aiden Mizhen <amizhen30@stuy.edu>
2022-04-26 22:39:57 -0400

    Poked around

--- power_fitness.py
+++ power_fitness.py
@@ -1,11 +1,24 @@
+from data.Data import get_solar_data, get_wind_data, get_electric_demand
 from power_system import PowerSys
+from datetime import date
 
-
-def test(sys : PowerSys, loc : str) -> None:
+def test(sys : PowerSys, loc : str, start : date, end : date) -> None:
     """
     :param sys: Takes a power setup
     :param loc: Takes a location
+    :param start: Start Date (find min/max)
+    :param end: End Date (find min/max)
     :return: modifies the fitness parameter of the input power system (maybe returns other useful info about run)
     """
+
+    # Need to convert location to lat and longitute cords, maybe region too.
+
+    lat = 42.355978782625336
+    lon = -74.9824086324889
+
+    # wind_data = get_wind_data(lat, lon, start, end)
+    # solar_data = get_solar_data(lat, lon, start, end)
+    # demand = get_electric_demand(loc, )
+
     return
 
