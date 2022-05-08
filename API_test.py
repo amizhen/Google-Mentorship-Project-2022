f0228ae379242ec1e54a78d837c331765bd4b27a
Aiden Mizhen <amizhen30@stuy.edu>
2022-05-08 18:18:21 -0400

    Started writing height conversion

--- API_test.py
+++ API_test.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 import pprint
-from data.Data import get_electric_demand_in_range, get_solar_data, get_wind_data, get_wind_data_in_range
+from data.Data import get_electric_demand, get_solar_data, get_wind_data, get_wind_data
 
 if __name__ == "__main__":
+    print('Solar')
     pprint.pprint(get_solar_data(42.355978782625336, -74.9824086324889, datetime(2007, 3, 1), datetime(2007, 3, 2)))
-
-    pprint.pprint(get_wind_data_in_range(42.355978782625336, -74.9824086324889, datetime(2007, 3, 1), datetime(2007, 3, 2)))
-
-    pprint.pprint(get_electric_demand_in_range("New York", datetime(2007, 3, 1), datetime(2007, 3, 2)))
+    print('Wind')
+    pprint.pprint(get_wind_data(42.355978782625336, -74.9824086324889, datetime(2007, 3, 1), datetime(2007, 3, 2)))
+    print('Demand')
+    pprint.pprint(get_electric_demand("New York", datetime(2007, 3, 1), datetime(2007, 3, 4)))
--- data/Data.py
+++ data/Data.py
@@ -9,37 +9,42 @@ import pandas as pd
 from util.WTKUtil import WTK_idx
 
 regions = {
-    "California" : "CAL", 
+    "California": "CAL",
     **dict.fromkeys(["North Carolina", "South Carolina"], "CAR"),
-    **dict.fromkeys(["North Dakota", "South Dakota", "Nebraska", "Kansas", "Oklahoma", "Minnesota"],"CENT"),
-    "Florida" : "FLA",
-    **dict.fromkeys(["Delaware", "Maryland", "New Jersey", "Pennsylvania", "Virginia", "West Virginia", "District of Columbia"], "MIDA"),
+    **dict.fromkeys(["North Dakota", "South Dakota", "Nebraska", "Kansas", "Oklahoma", "Minnesota"], "CENT"),
+    "Florida": "FLA",
+    **dict.fromkeys(
+        ["Delaware", "Maryland", "New Jersey", "Pennsylvania", "Virginia", "West Virginia", "District of Columbia"],
+        "MIDA"),
     **dict.fromkeys(["Illinois", "Indiana", "Iowa", "Michigan", "Ohio", "Wisconsin", "Missouri"], "MIDW"),
     **dict.fromkeys(["Connecticut", "Maine", "Massachusetts", "New Hampshire", "Rhode Island", "Vermont"], "NE"),
-    "New York" : "NY",
+    "New York": "NY",
     **dict.fromkeys(["Oregon", "Washington", "Idaho", "Montana", "Wyoming"], "NW"),
     **dict.fromkeys(["Alabama", "Georgia", "Arkansas", "Kentucky", "Louisiana", "Mississippi"], "SE"),
     **dict.fromkeys(["Arizona", "New Mexico", "Utah", "Nevada", "Colorado"], "SW"),
-    "Tennessee" : "TEN",
-    "Texas" : "TEX"  
+    "Tennessee": "TEN",
+    "Texas": "TEX"
 }
 
 with open("key.txt", "r") as file:
     API_KEY = file.read(40)
 
 # So it only opens onces
-WTK = h5pyd.File("/nrel/wtk-us.h5", "r", endpoint="https://developer.nrel.gov/api/hsds", api_key = API_KEY)
+WTK = h5pyd.File("/nrel/wtk-us.h5", "r", endpoint="https://developer.nrel.gov/api/hsds", api_key=API_KEY)
 
-def get_solar_data(lat : float, lon : float, start : datetime, end : datetime):
+
+def get_solar_data(lat: float, lon: float, start: datetime, end: datetime):
     end = end - timedelta(days=1);
-    response = requests.get(f"https://power.larc.nasa.gov/api/temporal/hourly/point?start={start.strftime('%Y%m%d')}&end={end.strftime('%Y%m%d')}&latitude={lat}&longitude={lon}&community=re&parameters=ALLSKY_SFC_LW_DWN&format=json&header=false&time-standard=utc")
+    response = requests.get(
+        f"https://power.larc.nasa.gov/api/temporal/hourly/point?start={start.strftime('%Y%m%d')}&end={end.strftime('%Y%m%d')}&latitude={lat}&longitude={lon}&community=re&parameters=ALLSKY_SFC_LW_DWN&format=json&header=false&time-standard=utc")
     if response.status_code == 200:
         data = response.json()["properties"]["parameter"]["ALLSKY_SFC_LW_DWN"]
-        return { datetime.strptime(datum, "%Y%m%d%H") : data[datum] for datum in data }
+        return {datetime.strptime(datum, "%Y%m%d%H"): data[datum] for datum in data}
     else:
         raise ValueError()
 
-def get_electric_demand(state : str, start : datetime, end : datetime):
+
+def get_electric_demand(state: str, start: datetime, end: datetime):
     """
     A function that retrieves electric demands of states. 
 
@@ -58,15 +63,17 @@ def get_electric_demand(state : str, start : datetime, end : datetime):
     response = requests.get(f"https://api.eia.gov/series/?api_key={API_KEY}&series_id=EBA.{regions[state]}-ALL.D.H")
     if (response.status_code == 200):
         data = response.json()
-        return { datetime.strptime(datum[0], "%Y%m%dT%HZ") : datum[1] for datum in data["series"][0]["data"] if start <= datetime.strptime(datum[0], "%Y%m%dT%HZ") < end }
+        return {datetime.strptime(datum[0], "%Y%m%dT%HZ"): datum[1] for datum in data["series"][0]["data"] if
+                start <= datetime.strptime(datum[0], "%Y%m%dT%HZ") < end}
     else:
         raise ValueError(f"Request Failed {response.status_code}")
 
-def get_wind_data(lat : float, lon : float, start : datetime, end : datetime):
+
+def get_wind_data(lat: float, lon: float, start: datetime, end: datetime):
     wtk_ij = WTK_idx(WTK, (lat, lon))
 
     epoch = datetime(2007, 1, 1)
-    hr = timedelta(hours = 1)
+    hr = timedelta(hours=1)
     print(int((start - epoch) / hr))
-    ds = WTK["windspeed_80m"][int((start - epoch)/hr):int((end - epoch)/hr):1, wtk_ij[0], wtk_ij[1]]
-    return {(start + hr * i) : ds[i] for i in range(len(ds))}
+    ds = WTK["windspeed_80m"][int((start - epoch) / hr):int((end - epoch) / hr):1, wtk_ij[0], wtk_ij[1]]
+    return {(start + hr * i): ds[i] for i in range(len(ds))}
