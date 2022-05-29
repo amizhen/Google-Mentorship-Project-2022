f967c962fae2fa9a77a60ce3784eb724eccdba4c
Aiden Mizhen <amizhen30@stuy.edu>
2022-05-29 19:42:26 -0400

    Moved PowerPlant and child classes to new file

---
+++ PowerPlant.py
@@ -0,0 +1,47 @@
+from combined_data import *
+from datetime import datetime
+
+
+class PowerPlant:
+    def __init__(self, location: tuple[float, float], amount: float, start: datetime, end: datetime, mode=None):
+        self.loc = location
+        self.amt = amount
+        self.mode = mode
+        self.start = start
+        self.end = end
+        self.data = self.fetch_data()
+
+    def tick(self, time: datetime):
+        return self.processData(time)
+
+    def fetch_data(self):
+        return NotImplemented
+
+    def processData(self, time: datetime):
+        return self.data[time] * self.amt
+
+    def __str__(self):
+        return f'{self.amt}, {self.mode} at {self.loc}'
+
+
+class WindPlant(PowerPlant):
+    def __init__(self, location: tuple[float, float], amount: int, start: datetime, end: datetime, radius: float = 35.0, height: float = 80.0):
+        super().__init__(location, amount, start, end, mode='wind')
+        self.radius = radius
+        self.height = height
+
+    def fetch_data(self):
+        return get_wind(self.start, self.end, self.loc[0], self.loc[1], self.radius, self.height)
+
+
+class SolarPlant(PowerPlant):
+    def __init__(self, location: tuple[float, float], amount: float, start: datetime, end: datetime, efficiency: float=0.15):
+        super().__init__(location, amount, start, end, mode='solar')
+        self.efficiency = efficiency
+
+    def fetch_data(self):
+        return get_solar(self.start, self.end, self.loc[0], self.loc[1], self.efficiency)
+
+
+if __name__ == '__main__':
+    pass
